e95a8f1470e450881d2e573f4511dfd053f4acfe
kareemismail <kareem.ismailmohamed@gmail.com>
2021-09-06 12:38:49 +0200

    Removed the web support files.

--- src/main/java/com/example/ecommerce/api/controller/ProductController.java
+++ src/main/java/com/example/ecommerce/api/controller/ProductController.java
@@ -1,46 +1,32 @@
 package com.example.ecommerce.api.controller;
 
-import com.example.ecommerce.api.enums.View;
-import com.example.ecommerce.service.dto.ProductDTO;
 import com.example.ecommerce.service.ProductService;
-import org.modelmapper.ModelMapper;
+import com.example.ecommerce.service.dto.ProductDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Map;
 
-@Controller
+@RestController
 public class ProductController {
 
     @Autowired
     private ProductService productService;
 
     @PostMapping("/addProduct")
-    public String addProduct(@RequestParam Map<String, Object> productMapValues) {
-        ModelMapper modelMapper = new ModelMapper();
-        ProductDTO productDTO = modelMapper.map(productMapValues, ProductDTO.class);
+    public String addProduct(@ModelAttribute ProductDTO productDTO) {
         productService.addProduct(productDTO);
-        return View.ADD_PRODUCT.getViewName();
-    }
-
-    @GetMapping("/addProduct")
-    public String addProductPage() {
-        return View.ADD_PRODUCT.getViewName();
+        return "product added";
     }
 
     @GetMapping("/deleteProduct")
     public String deleteProduct(@RequestParam String productId) {
         productService.deleteProduct((long)Integer.parseInt(productId));
-        return View.PRODUCTS_REDIRECT.getViewName();
+        return "Succeeded";
     }
 
     @GetMapping("/getAllProducts")
-    public String getAllProducts(Model model) {
-        List<ProductDTO> allProducts = productService.getAllProducts();
-        model.addAttribute("products", allProducts);
-        return View.PRODUCTS.getViewName();
+    public List<ProductDTO> getAllProducts() {
+        return productService.getAllProducts();
     }
 }
